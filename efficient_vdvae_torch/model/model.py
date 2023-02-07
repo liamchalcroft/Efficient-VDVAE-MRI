218354b3ae70abbaa46ea45ea880e4a22d7722a4
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2023-02-07 10:00:00 +0000

    Fix device issue

--- efficient_vdvae_torch/model/model.py
+++ efficient_vdvae_torch/model/model.py
@@ -388,8 +388,7 @@ def _compiled_train_step(model, inputs, step_n):
 
 
 def _compiled_amp_train_step(model, inputs, step_n, scaler, optimizer):
-    print(str(inputs.device))
-    with torch.autocast(str(inputs.device)):
+    with torch.autocast("cuda" if "cuda" in str(inputs.device) else str(inputs.device)):
         predictions, posterior_dist_list, prior_kl_dist_list = model(inputs)
         (
             avg_feature_matching_loss,
