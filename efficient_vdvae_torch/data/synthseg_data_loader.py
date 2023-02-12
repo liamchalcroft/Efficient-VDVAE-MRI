424f574baa33118bd6cdabba9c3baf8614663f57
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2023-02-12 20:21:15 +0000

    Same again

--- efficient_vdvae_torch/data/synthseg_data_loader.py
+++ efficient_vdvae_torch/data/synthseg_data_loader.py
@@ -105,9 +105,9 @@ def labels_to_image(path_list, mean_list=None, std_list=None):
             for (lab, std, mean) in zip(all_labels, mean_list, std_list)
         ]
     )
-    image -= float(image.min())
-    image /= float(image.max())
-    image *= 255
+    image = image / float(image.min())
+    image = image / float(image.max())
+    image = image * 255
     return Image.fromarray(np.uint8(image))
 
 
